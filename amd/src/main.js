1279c09e84e11d4440fdbd6b9d7cc0330e279b47
sokunthearithmakara <smakara@hawaii.edu>
2025-01-13 14:28:27 +0700

    STABLE1.0

--- amd/src/main.js
+++ amd/src/main.js
@@ -35,57 +35,14 @@ export default class OfficeViewer extends Iframe {
         $message.addClass("hasiframe");
         super.renderContainer(annotation);
     }
-    /**
-     * Runs the interaction for the given annotation.
-     *
-     * @param {Object} annotation - The annotation object containing interaction details.
-     * @param {number} annotation.id - The unique identifier for the annotation.
-     * @param {boolean} annotation.completed - Indicates if the annotation has been completed.
-     * @param {number} annotation.hascompletion - Indicates if the annotation has completion tracking.
-     * @param {string} annotation.completiontracking - The type of completion tracking for the annotation.
-     * @param {string} annotation.displayoptions - The display options for the annotation.
-     *
-     * @returns {Promise<void>} - A promise that resolves when the interaction is complete.
-     */
-    async runInteraction(annotation) {
-        await this.player.pause();
 
+    async applyContent(annotation) {
+        super.applyContent(annotation);
         // Check if device is online.
         if (!navigator.onLine) {
             this.addNotification('You need to be online to view this content.');
             this.player.play();
             return;
-        }
-
-        let self = this;
-
-        // Apply content.
-        const applyContent = async(annotation) => {
-            const data = await this.render(annotation, 'html');
-            $(`#message[data-id='${annotation.id}'] .modal-body`).attr('id', 'content').html(data).fadeIn(300);
-            if (annotation.hascompletion == 0 || annotation.completed) {
-                this.postContentRender(annotation);
-                return;
-            }
-            if (annotation.completiontracking == 'view') {
-                this.postContentRender(annotation);
-                this.toggleCompletion(annotation.id, "mark-done", "automatic");
-                return;
-            }
-        };
-
-        await this.renderViewer(annotation);
-        this.renderContainer(annotation);
-        applyContent(annotation);
-
-        if (annotation.displayoptions == 'popup') {
-            $('#annotation-modal').on('shown.bs.modal', function() {
-                self.setModalDraggable('#annotation-modal .modal-dialog');
-            });
-        }
-
-        if (annotation.completiontracking == 'manual') {
-            this.enableManualCompletion(annotation);
         }
     }
 }
